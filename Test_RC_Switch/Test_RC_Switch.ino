53ff40690164bf2b960dcd4d702a54895b805079
Aquafish007 <Volkermi@gmx.de>
2014-08-27 20:59:07 +0200

    Some corrections in EEPROM EEPROM_clear added Test_RC_Switch added

---
+++ Test_RC_Switch/Test_RC_Switch.ino
@@ -0,0 +1,26 @@
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+
+  // Transmitter is connected to Arduino Pin #10  
+  mySwitch.enableTransmit(38); //10
+  
+}
+
+void loop() {
+  mySwitch.switchOn("10000", 5);
+  
+
+  // Wait a second
+  delay(2000);
+  
+  // Switch off
+  mySwitch.switchOff("10000", 5);
+  
+  
+  // Wait another second
+  delay(2000);
+  
+}
---
+++ eeprom_clear/eeprom_clear.ino
@@ -0,0 +1,27 @@
+/*
+ * EEPROM Clear
+ *
+ * Sets all of the bytes of the EEPROM to 0.
+ * This example code is in the public domain.
+
+ */
+
+#include <EEPROMex.h>
+
+const int memBase = 0;
+
+void setup()
+{
+  EEPROM.setMemPool(memBase, EEPROMSizeMega);
+  
+  // write a 0 to all 512 bytes of the EEPROM
+  for (int i = 0; i < 4096; i++)
+    EEPROM.write(i, 0);
+    
+  // turn the LED on when we're done
+  digitalWrite(6, HIGH);
+}
+
+void loop()
+{
+}
